b783a53833c18a623d1dd7768d5554236d168679
Ned-el-ch <nedelchev.niki@gmail.com>
2019-11-22 13:05:27 +0000

    stuff

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,67 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    loadTrainers();
+
+})
+
+function loadTrainers() {
+
+    fetch(TRAINERS_URL)
+    .then(res => res.json())
+    .then(res => {
+
+        res.forEach( element => {
+
+            createCard(element)
+
+        });
+
+    });
+
+};
+
+function createCard(obj) {
+
+    const cardContainer = document.querySelector('main')
+
+    let div = document.createElement('div');
+    div.classList.add('card');
+    div.dataset.id = obj.id
+
+    let p = document.createElement('p')
+    p.textContent = obj.name
+
+    let addPokemonButton = document.createElement('button');
+    addPokemonButton.dataset.trainerId = obj.id
+    addPokemonButton.textContent = 'Add Pokemon'
+    addPokemonButton.style.outline = 'none';
+
+    let ul = document.createElement('ul');
+
+    for (element of obj.pokemons) {
+        // console.log(element)
+        const li = document.createElement('li');
+        // debugger;
+        li.textContent = `${element.nickname} (${element.species}) `;
+
+        const releaseButton = document.createElement('button');
+        releaseButton.textContent = 'Release';
+        releaseButton.className = 'release';
+        releaseButton.dataset.PokemonId = element.id;
+        releaseButton.style.outline = 'none';
+
+        li.appendChild(releaseButton);
+        ul.appendChild(li);
+
+    }
+
+    div.appendChild(p);
+    div.appendChild(addPokemonButton);
+    div.appendChild(ul);
+    cardContainer.appendChild(div);
+
+}
